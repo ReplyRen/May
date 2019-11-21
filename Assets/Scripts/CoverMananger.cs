205855e498a55c4fa61e4204260019921844c336
方政言 <1561684239@qq.com>
2019-11-21 11:50:52 +0800

    方政言
    封面完成

---
+++ Assets/Scripts/CoverMananger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverMananger : MonoBehaviour
+{
+    public GameObject cover;
+    public GameObject op;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        op.SetActive(false);
+        cover.SetActive(true);
+    }
+
+    public void startbutton()
+    {
+        Debug.Log("press button");
+        op.SetActive(true);
+        cover.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
