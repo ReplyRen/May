9fbd8c8eba7baad2af99b42a0bc243a2693d14b1
yupeiqi <37902799+ciyuanzhaizu@users.noreply.github.com>
2019-11-22 17:18:44 +0800

    于沛琦更新
    基本完成，需要测试
    图标的修改还得和其他程序一起对接
    基本数值的增加删减完成了

--- Assets/Scripts/BuildingUI.cs
+++ Assets/Scripts/BuildingUI.cs
@@ -7,7 +7,7 @@ public class BuildingUI : MonoBehaviour
 {
     /*要更换的图片*/
     public Sprite imageChange;
-    
+
     /*当前图片*/
     public Image imageNow;
     public Image imageOther;
@@ -16,25 +16,57 @@ public class BuildingUI : MonoBehaviour
     public int buttonNum;
     private int _buttonNum;
     /*图片序列*/
-    private int[] picNum =new int[9];
+    private int[] picNum = new int[9];
     private int[] buttonLevel = new int[3];
     /*确认窗口*/
     [SerializeField]
-    private Transform confirmWin;
+    private Transform confirmWindows;
     private bool buildChange;
+    /*获取Player资源*/
+    public GameObject player;
+    private int flag;
+    private int player_chip;
+    private int player_Resource;
+    private int player_Electric;
+    public Text ChipText;
+    public Text ResourceText;
+    public Text ElectricText;
+    public Text TipText;
+    private string[] textBuild = new string[9] { "干扰装置（level 1）：10s内不会受到装置作用范围内怪物的伤害\n装置CD：20步\n建造消耗：1芯片+100物资+100电力",
+        "干扰装置（level 2）：10s内不会受到装置作用范围内怪物的伤害，作用范围扩大6格\n装置CD：20步\n建造消耗：1芯片+100物资+100电力",
+        "干扰装置（level 3）：10s内不会受到装置作用范围内怪物的伤害，作用范围扩大6格\n装置CD：20步\n建造消耗：1芯片+100物资+100电力",
+        "克隆装置（level 1）：20s内使选定格子资源增加1倍，若含有怪物则数量同样增加；当前等级可克隆1次\n装置CD：16步\n建造消耗：1芯片+140物资+60电力",
+        "克隆装置（level 2）：20s内使选定格子资源增加1倍，若含有怪物则数量同样增加；当前等级可克隆2次（可选择同一格子）\n装置CD：16步\n建造消耗：1芯片+140物资+60电力",
+        "克隆装置（level 3）：20s内使选定格子资源增加1倍，若含有怪物则数量同样增加；当前等级可克隆3次（可选择同一格子）\n装置CD：16步\n建造消耗：1芯片+140物资+60电力",
+        "探测装置（level 1）：可在物资/电力/医疗包/芯片/怪物中选择任一类型格子进行探测，当前等级持续时间为10s\n装置CD：25步\n建造消耗：1芯片+60物资+140电力",
+        "探测装置（level 2）：可在物资/电力/医疗包/芯片/怪物中选择任一类型格子进行探测，当前等级持续时间为20s\n装置CD：25步\n建造消耗：1芯片+60物资+140电力",
+        "探测装置（level 3）：可在物资/电力/医疗包/芯片/怪物中选择任一类型格子进行探测，当前等级持续时间为30s\n装置CD：25步\n建造消耗：1芯片+60物资+140电力"};
+    private string[] textRemove = new string[3] {"确定拆除该装置？\n（返还1个芯片、 50个物资、 50个电力）",
+        "确定拆除该装置？\n（返还1个芯片、 70个物资、 30个电力）",
+        "确定拆除该装置？\n（返还1个芯片、 70个物资、 30个电力）"};
+    /*消耗的资源*/
+    private int[] costPlayer = new int[9] { 1, 100, 100, 1, 140, 60, 1, 60, 140 };
     // Start is called before the first frame update
     void Start()
     {
         //imageNow = this.transform.GetChild(buttonNum).GetComponent<Image>();
         //imageNow = this.transform.GetComponent<Image>();
-        foreach(int i in picNum)
+        foreach (int i in picNum)
         {
             picNum[i] = 0;
         }
+        picNum[6] = 1;
+        picNum[7] = 1;
+        picNum[8] = 1;
         foreach (int i in buttonLevel)
         {
             buttonLevel[i] = 0;
         }
+
+        player = GameObject.FindWithTag("Player");
+        player_chip = player.GetComponent<PlayerAsset>().Chip;
+        player_Resource = player.GetComponent<PlayerAsset>().Resource;
+        player_Electric = player.GetComponent<PlayerAsset>().Electric;
     }
 
     // Update is called once per frame
@@ -42,7 +74,38 @@ public class BuildingUI : MonoBehaviour
     {
         //Debug.Log(buttonNum);
         //imageNow = this.transform.GetChild(buttonNum).GetComponent<Image>();
-        
+        /*显示文本的更新*/
+        player.GetComponent<PlayerAsset>().Chip = player_chip;
+        player.GetComponent<PlayerAsset>().Resource = player_Resource;
+        player.GetComponent<PlayerAsset>().Electric = player_Electric;
+        ChipText.text = player_chip.ToString();
+        ResourceText.text = player_Resource.ToString();
+        ElectricText.text = player_Electric.ToString();
+        /*装置图标切换*/
+        if (buttonLevel[0] > 0)
+        {
+            GameObject.FindWithTag("Interference").GetComponent<Device>().Unlock = false;
+        }
+        else
+        {
+            GameObject.FindWithTag("Interference").GetComponent<Device>().Unlock = true;
+        }
+        if (buttonLevel[1] > 0)
+        {
+            GameObject.FindWithTag("Clone").GetComponent<Device>().Unlock = true;
+        }
+        else
+        {
+            GameObject.FindWithTag("Clone").GetComponent<Device>().Unlock = true;
+        }
+        if (buttonLevel[2] > 0)
+        {
+            GameObject.FindWithTag("Probe").GetComponent<Device>().Unlock = true;
+        }
+        else
+        {
+            GameObject.FindWithTag("Probe").GetComponent<Device>().Unlock = true;
+        }
     }
 
     /// <summary>
@@ -51,31 +114,33 @@ public class BuildingUI : MonoBehaviour
     /// <param name="imageName"></param>
     public void GetImage(int imageName)
     {
-
-        if (picNum[_buttonNum] <= 2)
+        TipText.text = "";
+        if (picNum[_buttonNum] == 1 && buttonLevel[_buttonNum % 3] == (2 - _buttonNum / 3))
         {
-            if (picNum[_buttonNum] == 1 && buttonLevel[_buttonNum % 3] == (2 - _buttonNum / 3))
+            flag = (_buttonNum % 3) * 3;
+            if (player_chip >= costPlayer[flag] && player_Resource >= costPlayer[flag + 1] && player_Electric >= costPlayer[flag + 2])
             {
-                //Debug.Log(_buttonNum % 3+"    "+buttonLevel[_buttonNum % 3] + "    " + (2 - _buttonNum / 3));
+
                 buildChange = true;
-                confirmWin.gameObject.SetActive(true);
+                Debug.Log(textBuild[(_buttonNum % 3) * 3 + _buttonNum / 3]);
+                confirmWindows.GetComponentInChildren<Text>().text = textBuild[(_buttonNum % 3) * 3 + 2 - _buttonNum / 3];
+                confirmWindows.gameObject.SetActive(true);
                 imageSend = imageName;
             }
-            else if(picNum[_buttonNum] == 2 && buttonLevel[_buttonNum % 3] == (3 - _buttonNum / 3))
+            else
             {
-                Debug.Log("LevelMax and Delete" + _buttonNum);
+                TipText.text = "资源不足";
+            }
+        }
+        else if (picNum[_buttonNum] == 2)
+        {
+            TipText.text = "设备工作中";
+            if (buttonLevel[_buttonNum % 3] == (3 - _buttonNum / 3))
+            {
                 buildChange = false;
-                confirmWin.gameObject.SetActive(true);
+                confirmWindows.GetComponentInChildren<Text>().text = textRemove[_buttonNum % 3];
+                confirmWindows.gameObject.SetActive(true);
                 imageSend = imageName;
-                
-            }
-            else if(picNum[_buttonNum] == 0)
-            {
-                imageName = imageName + picNum[_buttonNum];
-                Debug.Log("build" + imageName.ToString());
-                imageChange = Resources.Load("build" + imageName.ToString(), typeof(Sprite)) as Sprite;
-                imageNow.sprite = imageChange;
-                picNum[_buttonNum]++;
             }
 
         }
@@ -95,48 +160,72 @@ public class BuildingUI : MonoBehaviour
     {
         if (resualt == true)
         {
-            confirmWin.gameObject.SetActive(false);
-            if (buttonLevel[_buttonNum % 3] < 3&&buildChange)
+            confirmWindows.gameObject.SetActive(false);
+            if (buttonLevel[_buttonNum % 3] < 3 && buildChange)
             {
                 imageSend = imageSend + picNum[_buttonNum];
                 imageChange = Resources.Load("build" + imageSend.ToString(), typeof(Sprite)) as Sprite;
                 imageNow.sprite = imageChange;
-                if (buttonLevel[_buttonNum % 3] > 0)
+                player_chip = player_chip - costPlayer[flag];
+                player_Resource = player_Resource - costPlayer[flag + 1];
+                player_Electric = player_Electric - costPlayer[flag + 2];
+                picNum[_buttonNum]++;
+                buttonLevel[_buttonNum % 3]++;
+
+                int imageSend1 = imageSend, imageSend2 = imageSend;
+                if (buttonLevel[_buttonNum % 3] < 3)//level高的改图
+                {
+                    imageOther = this.transform.GetChild(_buttonNum - 3).GetComponent<Image>();
+                    imageSend2 = imageSend2 + 2;
+                    imageChange = Resources.Load("build" + imageSend2.ToString(), typeof(Sprite)) as Sprite;
+                    imageOther.sprite = imageChange;
+                    picNum[_buttonNum - 3]++;
+                }
+                if (buttonLevel[_buttonNum % 3] > 0)//level低的改图
                 {
                     imageOther = this.transform.GetChild(_buttonNum + 3).GetComponent<Image>();
-                    imageSend = imageSend - 2;
-                    Debug.Log("imageother" + imageSend);
-                    imageChange = Resources.Load("build" + imageSend.ToString(), typeof(Sprite)) as Sprite;
+                    imageSend1 = imageSend1 - 2;
+                    imageChange = Resources.Load("build" + imageSend1.ToString(), typeof(Sprite)) as Sprite;
                     imageOther.sprite = imageChange;
+                    picNum[_buttonNum + 3]++;
                 }
-                picNum[_buttonNum]++;
-
-                buttonLevel[_buttonNum % 3]++;
             }
             if (buttonLevel[_buttonNum % 3] > 0 && !buildChange)
             {
-                picNum[_buttonNum] = 0;
-                imageSend = imageSend + picNum[_buttonNum];
-                Debug.Log(imageSend);
+                picNum[_buttonNum] = 1;
+                imageSend = imageSend + picNum[_buttonNum] - 1;
                 imageChange = Resources.Load("build" + imageSend.ToString(), typeof(Sprite)) as Sprite;
                 imageNow.sprite = imageChange;
-                if (buttonLevel[_buttonNum % 3] > 1)
+                player_chip = player_chip + costPlayer[flag];
+                player_Resource = player_Resource + costPlayer[flag + 1];
+                player_Electric = player_Electric + costPlayer[flag + 2];
+                buttonLevel[_buttonNum % 3]--;
+
+                int imageSend1 = imageSend;
+                if (buttonLevel[_buttonNum % 3] > 0)//level低的改图
                 {
                     imageOther = this.transform.GetChild(_buttonNum + 3).GetComponent<Image>();
-                    imageSend = imageSend - 2;
-                    Debug.Log(imageSend);
+                    imageSend1 = imageSend1 - 2;
+                    imageChange = Resources.Load("build" + imageSend1.ToString(), typeof(Sprite)) as Sprite;
+                    imageOther.sprite = imageChange;
+                    picNum[_buttonNum + 3]--;
+                }
+                if (buttonLevel[_buttonNum % 3] < 3)//level高的改图
+                {
+                    imageOther = this.transform.GetChild(_buttonNum - 3).GetComponent<Image>();
+                    imageSend = 3 - (_buttonNum % 3);
                     imageChange = Resources.Load("build" + imageSend.ToString(), typeof(Sprite)) as Sprite;
                     imageOther.sprite = imageChange;
+                    picNum[_buttonNum - 3] = 0;
                 }
-                buttonLevel[_buttonNum % 3]--;
-                Debug.Log(buttonLevel[_buttonNum % 3]);
             }
 
         }
         else
         {
-            confirmWin.gameObject.SetActive(false);
+            confirmWindows.gameObject.SetActive(false);
         }
     }
-    
+
+
 }
