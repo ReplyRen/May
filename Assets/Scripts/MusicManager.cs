5bc323689f7df6ba3e03d3940611b796c2213e52
yupeiqi <37902799+ciyuanzhaizu@users.noreply.github.com>
2019-11-28 10:29:03 +0800

    于沛琦修改
    添加音效

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static MusicManager _instance;
+
+    public AudioSource BGMSource;
+    public AudioSource AudioSource;
+
+    public AudioClip[] musicSource;
+    private static GameObject gamePlayAudio;
+
+    public static MusicManager getInstance()
+    {
+        if (_instance == null)
+        {
+            GameObject temp = MusicManager.gamePlayAudio;
+            _instance = temp.GetComponentInChildren<MusicManager>();
+        }
+        return _instance;
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        BGMSource = gameObject.AddComponent<AudioSource>();
+        AudioSource = gameObject.AddComponent<AudioSource>();
+    }
+    void Start()
+    {
+        BGMSource.clip = musicSource[0];
+        BGMSource.loop = true;
+        BGMSource.Play();
+    }
+
+    public void PlayMusicOnce(int MusicNum)
+    {
+        AudioSource.clip = musicSource[MusicNum];
+        AudioSource.Play();
+    }
+}
